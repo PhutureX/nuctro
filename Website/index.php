df692a3a43a895ab8f7103466b9ae9216a001c51
PhutureX <christoph.reich@gmx.at>
2017-09-18 14:09:24 +0200

    small url changes in .htaccess & index.php for clearer navigation ("/" on the end now bring an error), (subsite url still needs to be done)

--- Website/index.php
+++ Website/index.php
@@ -12,19 +12,19 @@ require "includes/functions.php";
 
 $page = ( isset($_GET['page'])) ? $_GET['page'] : "Frontpage";
 
-if($page == "Frontpage"){
+if($page  == "Frontpage" or $page == "frontpage"){
     $site = "sites/frontpage.php";
-}elseif($page == "Dashboard") {
+}elseif($page == "Dashboard" or $page == "dashboard") {
     $site = "sites/dashboard.php";
-}elseif($page == "MySubsites") {
+}elseif($page == "MySubsites" or $page == "mysubsites") {
     $site = "sites/subsites.php";
-}elseif($page == "Friends") {
+}elseif($page == "Friends" or $page == "friends") {
     $site = "sites/friendlist.php";
-}elseif($page == "Messages") {
+}elseif($page == "Messages" or $page == "messages") {
     $site = "sites/messages.php";
-}elseif($page == "Settings") {
+}elseif($page == "Settings" or $page == "settings") {
     $site = "sites/settings.php";
-}elseif($page == "Create") {
+}elseif($page == "Create" or $page == "create") {
     $site = "sites/create_subsite.php";
 }else{
     $site= "sites/404.php";
